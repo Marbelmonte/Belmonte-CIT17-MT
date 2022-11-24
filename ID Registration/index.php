c1cef3ba6802b1947fa27daf0c94554f9c226f73
Reymar Belmonte <45085928+Marbelmonte@users.noreply.github.com>
2022-11-25 01:22:09 +0800

    Add files via upload

---
+++ ID Registration/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<center><img src= "uc.png" height="150px" width="300px"></center>
+<meta charset utf-8
+<meta name= "viewpoint" content = "width= device-width, initial - scale = 1">
+<title>ID</title>
+<style>
+body{
+
+  background-color: #00FF7F
+
+</style>
+</p>
+</head>
+<h1><center><p style="font-family:verdana">ID REGISTRATION FORM</center></h1><br></p>
+<form action= "https://mystudentportal.uc-bcf.edu.ph">
+<h4><p style="font-family:verdana">STUDENT INFORMATION </br> 
+<h4><p style="font-family:verdana">Last Name: </br> 
+<input type="text name= Lname" size= "50"
+maxlength="20"><br></center></h4></p>
+<h4><p style="font-family:verdana">First Name: </br>
+<input type="text" name="Fname"size= "50"
+maxlength="20"><br></h4></p>
+<h4><p style="font-family:verdana">Middle Initial: </br>
+<input type="text" name="Lname"size= "4"
+maxlength="20"><br></h4></p>
+<h4><p style="font-family:verdana">Address: </br>
+<input type="text" name= "Address"size= "50"
+maxlength="40"><br></h4></p>
+
+<h2>
+<p style="font-family:verdana">Type your ID Number with Dash: <input type="text name= fname" size= "25"
+maxlength="15"><br></p>
+
+<h3><p style="font-family:verdana">Department of CITCS Course Major</h3></p>
+<select name= "course">
+<option selected= "selected">Web Development</option>
+<option>Network Security</option>
+<option>Computer Science</option>
+<option>Data Analytic</option>
+<option>Entrepreneurship</option>
+</br>
+</select>
+<h4><p style="font-family:verdana">Contact Number:
+</br><input type="text name= fname" size= "20"
+maxlength="12"><br></p></h4>
+
+<input type="submit" name="search">
+
+</select>
+</form>
+</body>
+</html>
